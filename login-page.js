4eba3d490137448aecfa1ce89bfb1563bd95a31e
TharllysonSilva <tharllysonsilva@gmail.com>
2021-07-02 11:17:15 -0300

    sms

--- login-page.js
+++ login-page.js
@@ -15,3 +15,18 @@ loginButton.addEventListener("click", (e) => {
         loginErrorMsg.style.opacity = 1;
     }
 })
+var data = "{\"Sender\":\"sender_id\",\"Receivers\":\"phone_number\",\"ContextRuleName\":\"rule_name\"}";
+
+  var xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+      console.log(this.responseText);
+    }
+  });
+
+  xhr.open("POST", "https://sms.comtele.com.br/api/v2/sendcontextmessage");
+  xhr.setRequestHeader("content-type", "application/json");
+  xhr.setRequestHeader("auth-key", "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX");
+
+  xhr.send(data);
