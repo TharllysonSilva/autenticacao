f8818408057dabe93c9fc2702d329eb27071222d
TharllysonSilva <tharllysonsilva@gmail.com>
2021-07-02 10:53:35 -0300

    contact

--- login-page.js
+++ login-page.js
@@ -7,7 +7,7 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev" && contact === "contact") {
+    if (username === "user" && password === "web_dev" && contato === "contato") {
         alert("You have successfully logged in.");
         location.reload();
     } else {
